5ae3204b5be167b0a1c138c1ddec70113045f696
pacheko-ke <patrickoluochex@gmail.com>
2025-03-30 13:32:59 +0300

    remove fetch in tenant

--- app/tenant/page.jsx
+++ app/tenant/page.jsx
@@ -1,9 +1,8 @@
-'use client'
+
 import React from 'react'
 
 export default async function page() {
-    const users=await fetch('localhost:3000/api/tenant')
-    console.log(users)
+   
   return (
     <div>Tenant</div>
   )
